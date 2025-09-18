16330f3643472b77bc4e53f239ead0257913b51e
Nadia Elmourabiti <elmourabiti.nadia@gmail.com>
2025-09-18 11:46:59 +0100

    add databse info

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined
-}
+const globalForPrisma = global as unknown as { prisma?: PrismaClient }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
-})
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasourceUrl: process.env.NODE_ENV === 'production'
+      ? process.env.PRISMA_DATABASE_URL
+      : process.env.DATABASE_URL,
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
